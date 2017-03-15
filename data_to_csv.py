0637b44aa2e0c27bc8560aeaf0ef17237a33a960
Rex Low <qiweilow950823@gmail.com>
2017-03-15 18:54:36 +0800

    added data to csv

---
+++ data_to_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('data.txt') as input_file:
+   lines = input_file.readlines()
+   newLines = []
+   for line in lines:
+      newLine = line.strip().split()
+      newLines.append( newLine )
+
+with open('output.csv', 'wb') as test_file:
+   file_writer = csv.writer(test_file)
+   file_writer.writerows( newLines )
