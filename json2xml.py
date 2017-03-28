2f1a46e92e65475219625a28cc7616ea5bd6dac6
Rex Low <qiweilow950823@gmail.com>
2017-03-28 12:20:19 +0800

    added json to xml

---
+++ json2xml.py
@@ -0,0 +1,30 @@
+import json
+from dicttoxml import dicttoxml
+from pprint import pprint
+
+with open('instadataset.json') as data_file:    
+    json_obj = json.load(data_file)
+    xml = dicttoxml(json_obj)
+    print(xml)
+
+def transform(json_obj, line_padding=""):
+    result_list = list()
+
+    json_obj_type = type(json_obj)
+
+    if json_obj_type is list:
+        for sub_elem in json_obj:
+            result_list.append(json2xml(sub_elem, line_padding))
+
+        return "\n".join(result_list)
+
+    if json_obj_type is dict:
+        for tag_name in json_obj:
+            sub_obj = json_obj[tag_name]
+            result_list.append("%s<%s>" % (line_padding, tag_name))
+            result_list.append(json2xml(sub_obj, "\t" + line_padding))
+            result_list.append("%s</%s>" % (line_padding, tag_name))
+
+        return "\n".join(result_list)
+
+    return "%s%s" % (line_padding, json_obj)
