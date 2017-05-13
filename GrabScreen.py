2581a8291680766535727ddd09f6fe2ca05671d2
Rex Low <qiweilow950823@gmail.com>
2017-05-13 10:53:20 +0800

    improve color

--- GrabScreen.py
+++ GrabScreen.py
@@ -6,10 +6,10 @@ import time
 last_time = time.time()
 
 while(True):
-  printscreen_pil = ImageGrab.grab()
+  screen = ImageGrab.grab(bbox=(0,0,1000,500))
   print('Loop took {} seconds'.format(time.time() - last_time))
   last_time = time.time()
-  cv2.imshow('window', np.array(printscreen_pil))
+  cv2.imshow('window', cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB))
   if cv2.waitKey(25) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
     break
